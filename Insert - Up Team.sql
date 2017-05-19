d114719ac34772d3b5fc657fcc25d7e8afe7a443
Vinícius Campos <vpcampos@outlook.com>
2017-05-19 00:07:31 -0300

    Nova Versão dos scripts do banco

--- Insert - Up Team.sql
+++ Insert - Up Team.sql
@@ -1,6 +1,6 @@
 Use upteam
 
-insert into tt_dificuldade (nme_dificuldade, dsc_dificuldade, mpt_exp_dificuldade) values
+insert into tt_dificuldade (nme_dificuldade, dsc_dificuldade, mtp_exp_dificuldade) values
 ('Easy Peasy','O mais fácil de todos, a famosa mamata', 0.75),
 ('Padawan', 'Dificuldade de iniciante, aquela faz aí rapidinho', 1),
 ('Chico Xavier', 'A dificuldade Médium', 1.2 ),
@@ -18,9 +18,6 @@ insert into tt_prioridade(nme_prioridade, mtp_exp_prioridade) values
 ('Média', 1.5),
 ('Alta', 2),
 ('Pra Ontem', 2.5)
-
-insert into tt_titulo (nme_titulo, dsc_titulo) values
-('', '')
 
 insert into tt_estado_tarefa(nme_estado_tarefa) values 
 ('Nova'),
